27b04d432907d396d5193b497ad5bdba6b1290db
Rafael <rafael.kenjik@gmail.com>
2026-07-05 01:04:18 -0300

    Delete superioes.js

--- superioes.js
+++
@@ -1,4 +0,0 @@
-const SUPERIORES_AUTORIZADOS = [
-    { nome: "kingdomicegamer", idDiscord: "617468134621708288" },
-    { nome: "Oficial_Teste", idDiscord: "111" } // Para testar rápido no painel
-];
